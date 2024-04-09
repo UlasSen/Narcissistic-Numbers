d83c71bd364cf5a9a40d92edc074f0a92f909be8
Dionachil <ulasennn@gmail.com>
2024-04-09 15:44:27 +0300

    Add project files.

---
+++ Narcissistic Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Narcissistic(375);
+    }
+
+    public static void Narcissistic(int x)
+    {
+        int temp = x;
+        Double result = 0;
+        while (temp > 0)
+        {
+            result += Math.Pow(temp % 10, 3);
+            
+            temp = temp / 10;
+            
+
+        }
+        if (result == x) {
+            Console.WriteLine(x + " is a narcissistic number.");
+                
+        }
+        else { Console.WriteLine(x + " is not a narcissistic number."); }
+        
+    }
+}
